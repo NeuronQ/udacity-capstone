89b607c231114a633289fb36a97c9b254b11f515
Neuron Q <io@neuronq.ro>
2018-10-24 23:45:10 +0200

    script for full run ohlc plus dow

---
+++ btc-5min-ohlc-dow-full-run.py
@@ -0,0 +1,129 @@
+# write code that works in both Python 2 (2.7+) and 3 (3.5+)
+from __future__ import absolute_import, division, print_function, unicode_literals
+try:
+    input = raw_input
+    range = xrange
+except NameError:
+    pass
+
+
+import csv
+import os
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+import keras
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+
+import pprint
+from IPython.display import display as dd
+from IPython.display import Image
+
+from lib.helpers import (
+    timing, print_shapes, fix_all_rngs,
+    date_str_to_ts
+)
+from lib.visualization import simple_plot
+from lib.walk_forward_predict import (
+    run_walk_forward_validation_rnn,
+    run_walk_forward_validation_arima
+)
+import lib.etl as etl
+from models.rnn import make_rnn_model_constructor
+from models.lr import LRModel
+
+
+pp = pprint.PrettyPrinter(indent=2).pprint
+
+
+# PARAMS:
+BC5M_FILENAME = './data_clean/btc.300s.cbpro.csv'
+OUT_FILENAME = './results/btc5min_ohlc_dow.csv'
+
+
+data5m = pd.read_csv(
+    BC5M_FILENAME,
+    index_col='timestamp',
+    parse_dates=['datetime']
+)
+print('> size:', len(data5m))
+data5m['weekday'] = data5m.datetime.dt.dayofweek
+dd(data5m.dtypes)
+dd(data5m.head())
+dd(data5m.tail())
+
+DEFAULT_PARAMS = {
+    # data to extract:
+    'data_df': data5m,
+    'features': ['close', 'weekday', 'open', 'high', 'low', 'volume'],
+    'train_sz': 25000,
+    'test_sz': 3000,
+    # data processing:
+    'normalize': 'seqs',  # 'seqs' | 'data'
+    'detrend': False,
+    # model and prediction arrangement:
+    'seq_len': 100,
+    'pred_len': 100,
+    'model_maker': make_rnn_model_constructor([100, 0.1, 50, 0.1]),
+    # training:
+    'epochs': 3,
+    'batch_size': 512,
+    'shuffle': False,
+    # experiment setup:
+    'times': 1,
+    'skip': False,
+    'fix_all_rngs_to': False,
+    'fix_rngs_before_each': False,
+    # plotting:
+    'plot': False,
+    'fig_size': (10, 8),
+    'fast': True,
+}
+
+CSV_FIELDS = (
+    'model_description',
+    'train_from',
+    'train_to',
+    'test_on',
+    'shuffle',
+    'normalize',
+    'training_loss',
+    'rmse',
+    'rmse_cp',
+    'dir_acc',
+)
+
+
+def ddump(s):
+    """Print to both notebook and system stdout."""
+    # os.write(1, s)
+    print(s)
+
+
+def full_run(idx_from, idx_to, write_csv_header=False):
+    train_sz = DEFAULT_PARAMS['train_sz']
+    test_sz = DEFAULT_PARAMS['test_sz']
+
+    out_file = open(OUT_FILENAME, 'ab')
+    csv_writer = csv.DictWriter(out_file, fieldnames=CSV_FIELDS)
+    if write_csv_header:
+        csv_writer.writeheader()
+
+    with timing('full run %d - %d' % (idx_from, idx_to)):
+        for i in range(idx_from, idx_to, test_sz):
+            params = dict(DEFAULT_PARAMS,
+                          from_i=i - train_sz)
+            ddump("\n   ------ RUN %d (from %s, train on %d, test on %d)\n" % (
+                i, params['from_i'], params['train_sz'], params['test_sz']))
+            with timing('run segment'):
+                out = run_walk_forward_validation_rnn(**params)
+                csv_writer.writerow(out)
+                out_file.flush()
+
+
+full_run(DEFAULT_PARAMS['train_sz'], len(data5m), True)
